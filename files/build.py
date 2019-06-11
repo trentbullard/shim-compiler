4e5bc5d5fd05258efac94b98ec51f9a9eb988ade
trent bullard <trent.bullard@dhsgroup.com>
2019-06-11 16:39:55 -0500

    apply many new scaffolds

--- files/build.py
+++ files/build.py
@@ -4,29 +4,32 @@ import re
 
 def build_page(content):
     file_string = ""
-    if content.items():
+    if isinstance(content, list):
+        for child in content:
+            file_string += build_page(child)
+    else:
         for key, value in content.items():
-            if key != 'locals':
-                try:
-                    file_string += open(f'files/shims/{key}.htm').read()
-                except:
-                    print(f'failed to open shim file {key}')
-                    return file_string
-                if re.findall('<!-- <\( replace \)> -->', file_string):
-                    file_string = file_string.replace(
-                        '<!-- <( replace )> -->', build_page(value))
-            else:
-                for local_key, local_value in value.items():
-                    if re.findall(f'<!-- <\( {local_key} \)> -->', file_string):
+            if key == 'locals':
+                continue
+            try:
+                file_string += open(f'files/shims/{key}.htm').read()
+            except:
+                print(f'failed to open shim file {key}')
+                continue
+            if re.findall('~~ replace ~~', file_string):
+                file_string = file_string.replace(
+                    '~~ replace ~~', build_page(value))
+            if 'locals' in value:
+                for local_key, local_value in value['locals'].items():
+                    special = f'~~ {local_key} ~~'
+                    if re.findall(special, file_string):
                         file_string = file_string.replace(
-                            f'<!-- <( {local_key} )> -->', local_value)
+                            f'~~ {local_key} ~~', local_value)
     return file_string
 
 
 layout = json.load(open('files/layout.json', 'r'))
-
 pages = layout['pages']
-file_names = list(pages.keys())
 
 for file_name, page_content in pages.items():
     with open(f'{file_name}.htm', 'w') as file:
